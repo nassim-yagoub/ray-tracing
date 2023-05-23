741fbf61f56154659bbf6bf7d73f1edb235e92bb
Nassim Yagoub <nassim.yagoub@illuin.tech>
2023-05-23 17:50:36 +0200

    feat: use ppm format and remove show image crate

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,21 @@
-use show_image::{create_window, ImageInfo, ImageView};
-use std::{thread, time};
+fn main() {
+    const IMAGE_HEIGHT: i32 = 256;
+    const IMAGE_WIDTH: i32 = 256;
 
-#[show_image::main]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let image_height: u8 = 255;
-    let image_width: u8 = 255;
+    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    //let pixels = 3 * image_width * image_height;
+    for i in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("\rLines remaining: {}", i);
+        for j in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
 
-    let mut pixel_data: [u8; 3 * 255 * 255] = [0; 3 * 255 * 255];
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
 
-    for i in 0..255 {
-        for j in 0..255 {
-            pixel_data[255 * 3 * i + 3 * j] = j as u8;
-            pixel_data[255 * 3 * i + 3 * j + 1] = i as u8;
-            pixel_data[255 * 3 * i + 3 * j + 2] = 64;
+            println!("{} {} {}", ir, ig, ib);
         }
     }
-
-    let image = ImageView::new(
-        ImageInfo::rgb8(image_width as u32, image_height as u32),
-        &pixel_data,
-    );
-
-    // Create a window with default options and display the image.
-    let window = create_window("image", Default::default())?;
-    window.set_image("image-002", image)?;
-
-    let ten_millis = time::Duration::from_millis(10000);
-    thread::sleep(ten_millis);
-
-    Ok(())
 }
