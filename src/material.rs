50b84b3f01f76e52ec7d1234ca48b4cc42c96eb6
Nassim Yagoub <nassim.yagoub@illuin.tech>
2023-06-22 17:15:07 +0200

    feat: make metals fuzzy

--- src/material.rs
+++ src/material.rs
@@ -30,18 +30,22 @@ impl Material for Lambertian {
 
 pub struct Metal {
     pub albedo: Color,
+    pub fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Metal {
-        Metal { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Metal {
+        Metal { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray, hit_record: HitRecord) -> (Color, Ray, bool) {
         let reflected = r_in.direction.unit_vector().reflect(hit_record.normal);
-        let scattered = Ray::new(hit_record.p, reflected);
+        let scattered = Ray::new(
+            hit_record.p,
+            reflected + self.fuzz * Vec3::random_in_unit_sphere(),
+        );
         let success = scattered.direction.dot(hit_record.normal) > 0.0;
 
         return (self.albedo, scattered, success);
